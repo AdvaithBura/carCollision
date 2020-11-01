5eaa29eeeb6c5561d381b1056f4111dc0565fa9c
AdvaithBura <72775491+AdvaithBura@users.noreply.github.com>
2020-11-01 14:24:46 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,16 @@
-
+var speed;
+var weight;
+var car;
+var wall;
 
 function setup() {
   createCanvas(1600,400);
   createSprite(400, 200, 50, 50);
 
- var speed = random(55,90);
- var weight = random(400,1500);
- var car = createSprite(50,200,50,50);
- var wall = createSprite(1500,200,60,height/2);
+  speed = random(55,90);
+  weight = random(400,1500);
+  car = createSprite(50,200,50,50);
+  wall = createSprite(1500,200,60,height/2);
 }
 
 function draw() {
